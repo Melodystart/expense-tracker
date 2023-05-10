0281ba42794f975d19ef3118c4b93ecb685e9dcd
Maji <start99start@gmail.com>
2023-05-10 23:48:57 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const mongoose = require('mongoose') // 載入 mongoose
 // 如果在 Heroku 環境則使用 process.env.PORT, 否則為本地環境，使用 3000 
 const PORT = process.env.PORT || 3000
 const exphbs = require('express-handlebars');
+const Record = require('./models/record') // 載入 Record model
+const Category = require('./models/category') // 載入 Category model
+const User = require('./models/user') // 載入 User model
 
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== 'production') {
@@ -30,7 +33,10 @@ app.set('view engine', 'hbs')
 
 // 設定首頁路由
 app.get('/', (req, res) => {
-  res.render('index')
+  Record.find() // 取出 Record model 裡的所有資料
+    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .then(records => res.render('index', { records: records })) // 將資料傳給 index 樣板
+    .catch(error => console.error(error)) // 錯誤處理
 })
 
 // 設定 port
